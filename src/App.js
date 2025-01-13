85358e77e409db104bffc7537b1f087fb4a0d39b
Alexander-Thukral <133746392+Alexander-Thukral@users.noreply.github.com>
2025-01-13 16:52:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PensionCalculator from "./PensionCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <PensionCalculator />
+    </div>
+  );
+}
+
+export default App;
